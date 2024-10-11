9cf5c27f1114787242e18f5615f0b3fd5a57233c
sharanu dhammur <sharandhammur@gmail.com>
2024-10-11 18:56:56 +0530

    removed some unwanted code and files

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -19,9 +19,8 @@ const MessageContainer = () => {
 				<NoChatSelected />
 			) : (
 				<>
-					{/* Header */}
 					<div className='bg-slate-500 px-4 py-3 rounded-tr-xl'>
-						{/* <span className='label-text'>To:</span>{" "} */}
+					{/* <div className='bg-slate-500 px-4 py-2 mb-2 rounded-tr-xl'> */}
 						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 					</div>
 					<Messages />
@@ -45,24 +44,3 @@ const NoChatSelected = () => {
 		</div>
 	);
 };
-
-// STARTER CODE SNIPPET
-// import MessageInput from "./MessageInput";
-// import Messages from "./Messages";
-
-// const MessageContainer = () => {
-// 	return (
-// 		<div className='md:min-w-[450px] flex flex-col'>
-// 			<>
-// 				{/* Header */}
-// 				<div className='bg-slate-500 px-4 py-2 mb-2'>
-// 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
-// 				</div>
-
-// 				<Messages />
-// 				<MessageInput />
-// 			</>
-// 		</div>
-// 	);
-// };
-// export default MessageContainer;
